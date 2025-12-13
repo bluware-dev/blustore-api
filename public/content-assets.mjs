3f7ebcb6fd10418a7ee0813bbdbec38ba3f6ec12
bluware-dev <bluware.contact@gmail.com>
2025-12-13 19:55:16 -0300

    chore(docs): agregar build estático de la documentación

---
+++ public/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_p3RAc from "../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx";
+export default new Map([["../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_p3RAc]]);
+		
