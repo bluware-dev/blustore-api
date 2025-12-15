98ecd744ee5602c447ecdb491b0fc07dc12dffc0
bluware-dev <bluware.contact@gmail.com>
2025-12-14 23:28:42 -0300

    chore(lint): aplicar linter y formatter a todo el proyecto

--- public/content-assets.mjs
+++ public/content-assets.mjs
@@ -1,4 +1,7 @@
-
-import __ASTRO_IMAGE_IMPORT_p3RAc from "../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx";
-export default new Map([["../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_p3RAc]]);
-		
+import __ASTRO_IMAGE_IMPORT_p3RAc from '../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx';
+export default new Map([
+	[
+		'../../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fdocs%2Findex.mdx',
+		__ASTRO_IMAGE_IMPORT_p3RAc,
+	],
+]);
